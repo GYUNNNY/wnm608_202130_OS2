eb44f1b857aee7fd6dec939682aaeae9427a29dc
Yun Yao <yaoyun0831@gmail.com>
2021-04-07 19:12:31 -0700

    add php files

---
+++ yao.yun/parts/navbar.php
@@ -0,0 +1,18 @@
+<header class="navbar">
+        <div class="container display-flex flex-aligh-center">
+           <div class="logo">
+              <a href="">
+                 <img src="images/logo1.png" width="100px">
+               </a>
+           </div>
+
+           <nav class="nav flex-none">
+              <ul class="display-flex">
+                 <li><a href="coffeelist.php">COFFEE</a></li>
+                 <li><a href="#giftslist.php">GIFTS</a></li>
+                 <li><a href="#ourstory.php">OUR STORY</a></li>
+                 <li><a href="#cafes.php">CAFÉS</a></li>
+              </ul>
+           </nav>
+        </div>
+   </header>
